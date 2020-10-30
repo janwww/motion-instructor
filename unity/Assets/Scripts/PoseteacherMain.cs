d37b415bf0f376bcf227cd1ff26b7e5f27ef688f
Felix <pffelix@student.ethz.ch>
2020-10-30 16:58:00 +0100

    integrated first draft for pose orientation similarity calculation

--- unity/Assets/Scripts/PoseteacherMain.cs
+++ unity/Assets/Scripts/PoseteacherMain.cs
@@ -443,6 +443,8 @@ class StickContainer : Container
     {
         stick.SetActive(active);
     }
+
+
 }
 
 // Class that contains information about the robot and joints contained in an AvatarGo object
@@ -849,6 +851,9 @@ class AvatarGo
             avatarContainer.transform.localScale = new Vector3(-1.0f, 1.0f, 1.0f);
         }
     }
+
+   
+
 }
 
 // Main script
@@ -1572,6 +1577,108 @@ public class PoseteacherMain : MonoBehaviour
         }
     }
 
+
+    // get similarity of pose between 2 avatars
+    double GetSimilarity(AvatarGo object1, AvatarGo object2)
+    {
+        // define all stick names (should be actually moved to a parameter file)
+        List<string> stickNames = new List<string>(new string[] {
+            "LLLeg",
+            "RLLeg",
+            "LeftUpperArm",
+            "RightUpperArm",
+            "LeftUpperLeg",
+            "RightUpperLeg",
+            "TorsoLeft",
+            "TorsoRight",
+            "HipStick",
+            "LeftLowerArm",
+            "RightLowerArm",
+            "LeftEye",
+            "RightEye",
+            "Shoulders",
+            "MouthStick",
+            "NoseStick",
+            "LeftEar",
+            "RightEar",
+            "LeftShoulderStick",
+            "RightShoulderStick",
+            "LeftHipStick",
+            "RightHipStick",
+            "LeftElbowStick",
+            "RightElbowStick",
+            "LeftWristStick",
+            "RightWristStick",
+            "LeftKneeStick",
+            "RightKneeStick",
+            "LeftAnkleStick",
+            "RightAnkleStick"
+        });
+        int stickNumber = stickNames.Count;
+
+        // weight each stick
+        List<double> stickWeights = new List<double>(new double[] {
+            1.0,
+            1.0,
+            1.0,
+            1.0,
+            1.0,
+            1.0,
+            1.0,
+            1.0,
+            1.0,
+            1.0,
+            1.0,
+            1.0,
+            1.0,
+            1.0,
+            1.0,
+            1.0,
+            1.0,
+            1.0,
+            1.0,
+            1.0,
+            1.0,
+            1.0,
+            1.0,
+            1.0,
+            1.0,
+            1.0,
+            1.0,
+            1.0,
+            1.0,
+            1.0
+        });
+        double stickWeightSum = 0.0;
+        foreach (double stickWeight in stickWeights)
+        {
+            stickWeightSum += stickWeight;
+        }
+
+        // get similarity (between 0 and 1) of orientation between all sticks
+        double similarity = 0.0;
+        for (int i = 0; i < stickNumber; i++)
+        {
+            // get position and orientation of relevant game objects
+            object1.smplContainer.smpl.gameObject.SetActive(true);
+            Vector3 vec1Position = GameObject.Find(stickNames[i]).transform.position;
+            Quaternion vec1Rotation = GameObject.Find(stickNames[i]).transform.rotation;
+            object1.smplContainer.smpl.gameObject.SetActive(false);
+            object2.smplContainer.smpl.gameObject.SetActive(true);
+            Vector3 vec2Position = GameObject.Find(stickNames[i]).transform.position;
+            Quaternion vec2Rotation = GameObject.Find(stickNames[i]).transform.rotation;
+            object2.smplContainer.smpl.gameObject.SetActive(false);
+
+            // get cosine similarity from quaternion 
+            // background: https://www.researchgate.net/publication/316447858_Similarity_analysis_of_motion_based_on_motion_capture_technology
+            // background: https://gdalgorithms-list.narkive.com/9TaVDT9G/quaternion-similarity-measure
+            double cos_angle = vec1Rotation.w * vec2Rotation.w + vec1Rotation.x * vec2Rotation.x + vec1Rotation.y * vec2Rotation.y + vec1Rotation.z * vec2Rotation.z;
+            cos_angle = Math.Abs(cos_angle);
+            similarity += cos_angle * stickWeights[i];
+        }
+        similarity /= stickWeightSum;
+        return similarity;
+    }
 
     // Done at each application update
     void Update()
