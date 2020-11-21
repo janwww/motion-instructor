d4b392d6db0c0dd2a93c64505085150c12b36125
Jan <wiegner.jan@gmail.com>
2020-11-21 14:56:51 +0100

    Move PoseInputGetter to separate file, cleanup main script

--- unity/Assets/Scripts/PoseteacherMain.cs
+++ unity/Assets/Scripts/PoseteacherMain.cs
@@ -8,293 +8,10 @@ using System.IO;
 using System.Collections;
 using NativeWebSocket;
 using System.Security.Permissions;
+using Microsoft.MixedReality.Toolkit.UI;
 
 namespace PoseTeacher
 {
-
-    // Label for various containers
-    public enum PoseInputSource
-    {
-        KINECT, WEBSOCKET, FILE
-    }
-
-    [CLSCompliant(false)]
-    public class PoseInputGetter
-    {
-
-        PoseInputSource CurrentPoseInputSource;
-        bool recording = false;
-        PoseData CurrentPose { get; set; }
-
-        // Azure Kinect variables
-        Device device;
-        BodyTracker tracker;
-
-        // Used for displaying RGB Kinect video
-        public Renderer videoRenderer;
-        RawImage m_RawImage;
-        GameObject streamCanvas;
-        GameObject videoCube;
-
-        //Select a Texture in the Inspector to change it (unused)
-        public Texture m_Texture;
-        Texture2D tex;
-
-        //Websocket variables
-        WebSocket websocket;
-        public string WS_ip = "ws://localhost:2567";
-        private PoseData poseLiveWS = null; // newest pose data from WS TODO: merge with CurrentPose
-
-        //File variables
-        IEnumerator<string> SequenceEnum;
-        private string _ReadDataPath;
-        public string ReadDataPath {
-            get { return _ReadDataPath; }
-            set { _ReadDataPath = value; LoadData(); } 
-        }
-        public string WriteDataPath { get; set; }
-        bool loadedData = false;
-
-        public PoseInputGetter(PoseInputSource InitPoseInputSource)
-        {
-            CurrentPoseInputSource = InitPoseInputSource;
-
-            switch (CurrentPoseInputSource)
-            {
-                case PoseInputSource.KINECT:
-                    StartAzureKinect();
-                    break;
-                
-                case PoseInputSource.WEBSOCKET:
-                    StartWebsocket();
-                    break;
-
-                case PoseInputSource.FILE:
-                    break;
-            }
-        }
-
-        private async void StartWebsocket()
-        {
-            // Contains code to instantiate a websocket to obtain pose data
-            // Web socket is currently used only for Kinect Alternative
-
-            websocket = new WebSocket(WS_ip);
-
-            websocket.OnOpen += () =>
-            {
-                Debug.Log("WS connection open!");
-            };
-
-            websocket.OnError += (e) =>
-            {
-                Debug.Log("Error! " + e);
-            };
-
-            websocket.OnClose += (e) =>
-            {
-                Debug.Log("WS connection closed!");
-            };
-
-            websocket.OnMessage += (bytes) =>
-            {
-                // If joint information is recieved, set poseLiveWS
-                var message = System.Text.Encoding.UTF8.GetString(bytes);
-                Debug.Log("WS message received: " + message);
-                var remote_joints = PoseDataUtils.DeserializeRJL(message);
-                Debug.Log(remote_joints);
-                poseLiveWS = PoseDataUtils.Remote2PoseData(remote_joints);
-            };
-
-            // Keep sending messages at every 0.3s
-            // TODO: if using websocket, make this work
-            //InvokeRepeating("SendWebSocketMessage", 0.0f, 0.3f);
-
-            await websocket.Connect();
-        }
-        // For testing websocket
-        async void SendWebSocketMessage()
-        {
-            if (websocket.State == WebSocketState.Open)
-            {
-                // Sending text
-                await websocket.SendText("Test message");
-            }
-        }
-
-        private void StartAzureKinect()
-        {
-            // Print to log
-            Debug.Log("Try loading device");
-            this.device = Device.Open(0);
-            var config = new DeviceConfiguration
-            {
-                ColorResolution = ColorResolution.r720p,
-                ColorFormat = ImageFormat.ColorBGRA32,
-                DepthMode = DepthMode.NFOV_Unbinned
-            };
-            device.StartCameras(config);
-
-            if (device != null)
-            {
-                Debug.Log("Found non-null device");
-                Debug.Log(device);
-            }
-
-            var calibration = device.GetCalibration(config.DepthMode, config.ColorResolution);
-
-            var trackerConfiguration = new TrackerConfiguration
-            {
-                SensorOrientation = SensorOrientation.OrientationDefault,
-                CpuOnlyMode = false
-            };
-            this.tracker = BodyTracker.Create(calibration, trackerConfiguration);
-            Debug.Log("Device loading finished");
-        }
-
-        private void LoadData()
-        {
-            //loadedData = true;
-            SequenceEnum = File.ReadLines(ReadDataPath).GetEnumerator();
-        }
-
-        // Appends the passed pose (PoseDataJSON format) to the file as JSON
-        void AppendRecordedFrame(PoseDataJSON jdl)
-        {
-            string json = JsonUtility.ToJson(jdl) + Environment.NewLine;
-            File.AppendAllText(WriteDataPath, json);
-        }
-        
-        // reset recording file
-        void ResetRecording()
-        {
-            File.WriteAllText(WriteDataPath, "");
-            Debug.Log("Reset recording file");
-        }
-
-
-        public PoseData GetNextPose()
-        {
-            switch (CurrentPoseInputSource)
-            {
-                case PoseInputSource.WEBSOCKET:
-                    #if !UNITY_WEBGL || UNITY_EDITOR
-                    websocket.DispatchMessageQueue();
-                    #endif
-                    // poseLiveWS is non-null if alternative is sending pose data over websocket
-                    if (poseLiveWS != null)
-                    {
-                        // Assign last pose from websocket
-                        // TODO: maybe animate in websocket code directly upon recieveing?
-                        CurrentPose = poseLiveWS;
-                    }
-                    else
-                    {
-                        Debug.Log("No pose recieved from WebSocket!");
-                        
-                    }
-                    break;
-
-                case PoseInputSource.FILE:
-                    // TODO: Only load once
-                    // Quick and dirty way to loop (by reloading file)
-                    if (SequenceEnum == null || !SequenceEnum.MoveNext())
-                    {
-                        LoadData();
-                        SequenceEnum.MoveNext();
-                    }
-
-                    string frame_json = SequenceEnum.Current;
-                    PoseData fake_live_data = PoseDataUtils.JSONstring2PoseData(frame_json);
-                    CurrentPose = fake_live_data;
-                    break;
-
-                case PoseInputSource.KINECT:
-                    if (device != null)
-                    {
-                        // TODO: move to function?
-                        // potentially simplify by not using "using" scopes
-                        using (Capture capture = device.GetCapture())
-                        {
-                            // Make tracker estimate body
-                            tracker.EnqueueCapture(capture);
-
-                            // Code for getting RGB image from camera
-                            var color = capture.Color;
-                            if (color != null && color.WidthPixels > 0)
-                            {
-                                UnityEngine.Object.Destroy(tex);// required to not keep old images in memory
-                                tex = new Texture2D(color.WidthPixels, color.HeightPixels, TextureFormat.BGRA32, false);
-                                tex.LoadRawTextureData(color.GetBufferCopy());
-                                tex.Apply();
-
-                                //Fetch the RawImage component from the GameObject
-                                if (streamCanvas != null)
-                                {
-                                    m_RawImage = streamCanvas.GetComponent<RawImage>();
-                                    if (m_RawImage != null)
-                                    {
-                                        m_RawImage.texture = tex;
-                                        videoRenderer.material.mainTexture = tex;
-                                    }
-                                }
-
-                            }
-                        }
-
-                        // Get pose estimate from tracker
-                        using (BodyFrame frame = tracker.PopResult())
-                        {
-                            Debug.LogFormat("{0} bodies found.", frame.BodyCount);
-
-                            //  At least one body found by Body Tracking
-                            if (frame.BodyCount > 0)
-                            {
-                                // Use first estimated person, if mutiple are in the image
-                                // !!! There are (probably) no guarantees on consisitent ordering between estimates
-                                var bodies = frame.Bodies;
-                                var body = bodies[0];
-
-                                // Apply pose to user avatar(s)
-                                PoseData live_data = PoseDataUtils.Body2PoseData(body);
-
-                                if (recording) // recording
-                                {
-                                    PoseDataJSON jdl = PoseDataUtils.Body2PoseDataJSON(body);
-                                    AppendRecordedFrame(jdl);
-                                }
-                                CurrentPose = live_data;
-                            }
-                        }
-                    }
-                    else
-                    {
-                        Debug.Log("device is null!");
-                    }
-                    break;
-                    
-            }
-            return CurrentPose;
-        }
-
-        public async void Dispose()
-        {
-            if (websocket != null)
-            {
-                await websocket.Close();
-            }
-            if (tracker != null)
-            {
-                tracker.Dispose();
-            }
-            if (device != null)
-            {
-                device.Dispose();
-            }
-        }
-
-    }
-
     // Main script
     [CLSCompliant(false)]
     public class PoseteacherMain : MonoBehaviour
@@ -331,20 +48,35 @@ namespace PoseTeacher
 
 
 
-        // File that is being read from
-        string current_file = "jsondata/2020_05_26-22_55_32.txt";
+        // Default file teacher (normally doesn't get used)
+        private const string teacherFilePath = "jsondata/2020_05_26-22_55_32.txt";
+        public string TeacherFilePath
+        {
+            get { return TeacherPoseInputGetter.ReadDataPath; }
+            set { TeacherPoseInputGetter.ReadDataPath = value; }
+        }
 
-        //For fake data 
-        //string fake_file = "jsondata/2020_05_26-21_23_28.txt";
-        //string fake_file = "jsondata/2020_05_26-22_55_32.txt";
-        string fake_file = "jsondata/2020_05_27-00_01_59.txt";
+        // For fake data 
+        private const string selfReadFilePath = "jsondata/2020_05_27-00_01_59.txt";
+        public string SelfReadFilePath
+        {
+            get { return SelfPoseInputGetter.ReadDataPath; }
+            set { SelfPoseInputGetter.ReadDataPath = value; }
+        }
+        //"jsondata/2020_05_26-21_23_28.txt"; "jsondata/2020_05_26-22_55_32.txt"; "jsondata/2020_05_27-00_01_59.txt";
 
         // can be changed in the UI
         // TODO: probaly change to using functions to toggle
         public bool isMaleSMPL = true;
-        public bool usingKinectAlternative = true;
-        public PoseInputSource SelfPoseInputSource = PoseInputSource.FILE;
+        //public bool usingKinectAlternative = true;
+        private PoseInputSource selfPoseInputSource = PoseInputSource.FILE;
+        public PoseInputSource SelfPoseInputSource
+        {
+            get { return SelfPoseInputGetter.CurrentPoseInputSource; }
+            set { SelfPoseInputGetter.CurrentPoseInputSource = value; }
+        }
 
+        
 
         public bool mirroring = true; // can probably be changed to private (if no UI elements use it)
 
@@ -365,8 +97,7 @@ namespace PoseTeacher
 
 
         // Mirror all avatar containers
-        // TODO: Move code to AvatarContainer class (partial done)
-        //      Conform to naming conventions
+        // TODO: Conform to naming conventions
         // Consider: what to do with videoCube and streamCanvas?
         //      Do we really need this function, or we will just set this one-by-one?
         public void do_mirror()
@@ -412,10 +143,6 @@ namespace PoseTeacher
         {
             isMaleSMPL = value;
         }
-        //public void set_OpenPose(bool value)
-        //{
-        //    usingKinectAlternative = value;
-        //}
         public void set_recording_mode(int rec)
         {
             recording_mode = rec;
@@ -427,10 +154,6 @@ namespace PoseTeacher
         public void set_playback_speed(int a)
         {
             playback_speed = a;
-        }
-        public void set_current_file(string curr)
-        {
-            current_file = curr;
         }
 
         // For setting in menu
@@ -454,12 +177,6 @@ namespace PoseTeacher
             }
         }
 
-        // Generate new filename with timestamp and set as file to write to
-        public void gen_new_current_file()
-        {
-            string timestamp = DateTime.Now.ToString("yyyy_MM_dd-HH_mm_ss");
-            current_file = "jsondata/" + timestamp + ".txt";
-        }
 
 
 
@@ -492,12 +209,19 @@ namespace PoseTeacher
             avatarListSelf[1].avatarContainer.gameObject.SetActive(false);
             avatarListTeacher[1].avatarContainer.gameObject.SetActive(false);
 
-            SelfPoseInputGetter = new PoseInputGetter(SelfPoseInputSource){ ReadDataPath = fake_file };
-            TeacherPoseInputGetter = new PoseInputGetter(PoseInputSource.FILE){ ReadDataPath = current_file};
+            SelfPoseInputGetter = new PoseInputGetter(selfPoseInputSource);
+            SelfReadFilePath = selfReadFilePath;
+            TeacherPoseInputGetter = new PoseInputGetter(PoseInputSource.FILE);
+            TeacherFilePath = teacherFilePath;
 
 
             // initialize similarity calculation instance and assign selected avatars
             avatarSimilarity = new AvatarSimilarity(avatarListSelf[similaritySelfNr], avatarListTeacher[similarityTeacherNr], similarityBodyNr);
+
+            // Button event from script test
+            var onClickReceiver = GameObject.Find("ButtonAlternative").GetComponent<Interactable>().GetReceiver<InteractableOnClickReceiver>();
+            onClickReceiver.OnClicked.AddListener(() => Debug.Log("ONtest"));
+            //onFocusReceiver.OnFocusOff.AddListener(() => Debug.Log("OFFtest"));
         }
 
 
