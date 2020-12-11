bd4be52de829eacd61aeed6c13721bf2b4cd42ea
ketjatekos <ketjatekos@users.noreply.github.com>
2020-12-11 17:11:51 +0100

    Mute button

---
+++ unity/Assets/Scripts/AudioAdjuster.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioAdjuster : MonoBehaviour
+{
+    //AudioListener listener;
+    //public GameObject holder;
+    private float soundLevel = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+      //  listener = holder.GetComponent<AudioListener>();    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void MuteOrUnmute()
+    {
+        if (AudioListener.volume != 0)
+        {
+            soundLevel = AudioListener.volume;
+            AudioListener.volume = 0;
+        } else
+        {
+            AudioListener.volume = soundLevel;
+        }
+    }
+}
