0fc36659d4a33bc694a083361e57802986eb261f
Utkarsh Bajpai <utkarsh.bajpai2015@vit.ac.in>
2021-01-07 20:53:22 +0530

    Commenting and cleaning up CoreoEndScreen

--- unity/Assets/Scripts/CoreoEndScreen.cs
+++ unity/Assets/Scripts/CoreoEndScreen.cs
@@ -11,9 +11,12 @@ namespace PoseTeacher {
         private int Score = -1;
         private int MaxScore = -1;
 
+        // The EndCoreoScreen GameObject
         public GameObject EndCoreoScreen;
         private GameObject CoreoScoreDescription;
+        // The CourseMenuHelper GameObject in Menu new
         public GameObject CourseHelper;
+        // The Main GameObject
         public GameObject Main;
 
         // Start is called before the first frame update
@@ -23,29 +26,34 @@ namespace PoseTeacher {
             EndCoreoScreen.SetActive(false);
         }
 
+        // Code that runs when the Show Recording button is pressed on the Coreo End Screen.
         public void ShowRecordingPressed()
         {
             Main.GetComponent<PoseteacherMain>().CoreoShowRestartRecording();
         }
 
+        // Code that runs when the Restart Coreography button is pressed on the Coreo End Screen.
         public void RestartCoreoPressed()
         {
             Main.GetComponent<PoseteacherMain>().RestartCoreo();
             EndCoreoScreen.SetActive(false);
         }
 
+        // Code that runs when the Next Move button is pressed on the Coreo End Screen.
         public void NextMovePressed()
         {
             CourseHelper.GetComponent<CourseMenuHelper>().StartNextStep();
             EndCoreoScreen.SetActive(false);
         }
 
+        // Sets and updates the name of the Coreography to the input string.
         public void SetCoreoName(string coreoName)
         {
             CoreoName = coreoName;
             UpdateText();
         }
 
+        // Sets and updates the current and max score of the Coreography according to the input values.
         public void SetScore(int score, int maxscore)
         {
             Score = score;
@@ -53,11 +61,13 @@ namespace PoseTeacher {
             UpdateText();
         }
 
+        // TODO Sets the rating (1 to 5 stars) of the currentyl played Coreography.
         public void SetRating()
         {
 
         }
 
+        // Updates the displayed text on the Coreo End Screen with the Coreography name and achieved score.
         public void UpdateText()
         {
             Text text = CoreoScoreDescription.GetComponent<Text>();
