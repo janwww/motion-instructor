0dcff94cd10fa64fdc82a748ea6ba2f6e9deb28f
softestsoph <43212194+softestsoph@users.noreply.github.com>
2021-11-09 11:48:13 +0100

    Add Dances + Adapt Scoring

--- unity/Assets/Scripts/Data/DancePerformanceScriptableObject.cs
+++ unity/Assets/Scripts/Data/DancePerformanceScriptableObject.cs
@@ -2,10 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using PoseTeacher;
+using System;
+
 
 [CreateAssetMenu(fileName = "Dance", menuName = "ScriptableObjects/DancePerformanceScriptableObject", order = 3)]
-    public class DancePerformanceScriptableObject : ScriptableObject {
+public class DancePerformanceScriptableObject : ScriptableObject {
     public DanceDataScriptableObject danceData;
+    public List<DanceDataScriptableObject> goals;
+    public List<float> goalTimestamps;
     public MusicDataScriptableObject SongObject;
     public DanceDifficulty Difficulty;
 }
