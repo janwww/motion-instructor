10529bc9ed730b5224fbdc8ca4877a8ee0659c78
softestsoph <43212194+softestsoph@users.noreply.github.com>
2021-11-22 12:12:28 +0100

    scoring manager + dance performance update
    make scoring manager a monobehaviour
    give danceperformance object option to do implicit goals

--- unity/Assets/Scripts/Data/DancePerformanceScriptableObject.cs
+++ unity/Assets/Scripts/Data/DancePerformanceScriptableObject.cs
@@ -3,13 +3,55 @@ using System.Collections.Generic;
 using UnityEngine;
 using PoseTeacher;
 using System;
-
+using UnityEditor;
 
 [CreateAssetMenu(fileName = "Dance", menuName = "ScriptableObjects/DancePerformanceScriptableObject", order = 3)]
 public class DancePerformanceScriptableObject : ScriptableObject {
     public DanceDataScriptableObject danceData;
-    public List<DanceDataScriptableObject> goals;
-    public List<float> goalTimestamps;
+    [Tooltip("If True, Goals are created dynamically based on timestamps")]
+    public bool dynamicGoals;
+    public List<float> goalStartTimestamps;
+    [Tooltip("Leave empty if dynamicGoals is true")]
+    [SerializeField] List<DanceDataScriptableObject> explicitGoals;
+    [Tooltip("Leave empty if dynamicGoals is false")]
+    [SerializeField] List<float> goalDuration;
     public MusicDataScriptableObject SongObject;
     public DanceDifficulty Difficulty;
+
+    public List<DanceData> goals {
+        get
+        {
+            List<DanceData> intermediateGoals = new List<DanceData>();
+            if (dynamicGoals)
+            {
+                DanceData decompressedDancedata = danceData.LoadDanceDataFromScriptableObject();
+                for (int i = 0; i < goalStartTimestamps.Count; i++)
+                {
+                    DanceData newDanceData = new DanceData();
+                    int id = 0;
+                    if (goalDuration[i] > 0)
+                    {
+                        for (float timestamp = goalStartTimestamps[i]; timestamp < goalStartTimestamps[i] + goalDuration[i]; timestamp += 0.1f)
+                        {
+                            newDanceData.poses.Add(decompressedDancedata.GetInterpolatedPose(0, out id, timestamp));
+                        }
+                    }
+                    else
+                    {
+                        newDanceData.poses.Add(decompressedDancedata.GetInterpolatedPose(0, out id, goalStartTimestamps[i]));
+                    }
+                    intermediateGoals.Add(newDanceData);
+                }
+            } else
+            {
+                foreach(DanceDataScriptableObject obj in explicitGoals)
+                {
+                    intermediateGoals.Add(obj.LoadDanceDataFromScriptableObject());
+                }
+            }
+            return intermediateGoals;
+        }
+    }
 }
+
+
